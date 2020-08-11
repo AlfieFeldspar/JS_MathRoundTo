8d18ff6639451693d69c8a28828b89fdcf424feb
Aissa Feldmann <aissaf@umich.edu>
2020-08-11 10:30:47 -0400

    removed console.logs

--- mathRoundTo.js
+++ mathRoundTo.js
@@ -1,10 +1,7 @@
 Math.roundTo = function (number, precision) {
   let converter = Math.pow(10, precision);
-  //console.log(converter);
   let multiplier = Math.round(number * converter);
-  //console.log(multiplier);
   let cutOff = Math.floor(multiplier);
-  //console.log(cutOff);
   let rounded = cutOff / converter;
   return rounded;
 };
